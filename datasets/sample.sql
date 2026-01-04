b1626bf246163173283d27eadc28c38bc91b39ef
sdutta787 <duttasoumyadeep592@gmail.com>
2026-01-04 16:39:54 +0530

    new changes

--- datasets/sample.sql
+++ datasets/sample.sql
@@ -1,2 +1,19 @@
 BEGIN TRANSACTION;
+CREATE TABLE "Account" (
+	id VARCHAR(255) NOT NULL, 
+	"Name" VARCHAR(255), 
+	"ParentId" VARCHAR(255), 
+	PRIMARY KEY (id)
+);
+INSERT INTO "Account" VALUES('Account-1','Sample Account for Entitlements','');
+INSERT INTO "Account" VALUES('Account-2','Test Data 1','');
+INSERT INTO "Account" VALUES('Account-3','Test Data 2','');
+CREATE TABLE "Delivery__c" (
+	id VARCHAR(255) NOT NULL, 
+	"Name" VARCHAR(255), 
+	"Scheduled_Date__c" VARCHAR(255), 
+	"Status__c" VARCHAR(255), 
+	"Supplier__c" VARCHAR(255), 
+	PRIMARY KEY (id)
+);
 COMMIT;
